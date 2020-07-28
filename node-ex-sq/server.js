79d2247c9b9a92ec667029f308066beebc57e502
bikash-chandra-behera <45479361+bikash-chandra-behera@users.noreply.github.com>
2020-07-28 09:25:48 -0700

    first_load
    Signed-off-by: bikash-chandra-behera <45479361+bikash-chandra-behera@users.noreply.github.com>

---
+++ node-ex-sq/server.js
@@ -0,0 +1,45 @@
+//server responsible for load express server
+//
+
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const db = require("./app/models");
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
+
+
+var corsOptions = {
+  origin: "http://localhost:8082"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+
+//add your all routs file
+require("./app/routes/tutorial.routes")(app);
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+})
